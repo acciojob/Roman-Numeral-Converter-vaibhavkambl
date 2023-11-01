a8298a6ee7485cb5cca4d72cc73ab97162231d74
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-11-01 12:13:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-function convertToRoman(num) {
-  if(num <= 0 || num >= 100000) {
-    return 'Invalid input';
-  }
-  const romanSymbols = [
-    { value: 1000, symbol: 'M' },
-    { value: 900, symbol: 'CM' },
-    { value: 500, symbol: 'D' },
-    { value: 400, symbol: 'CD' },
-    { value: 100, symbol: 'C' },
-    { value: 90, symbol: 'XC' },
-    { value: 50, symbol: 'L' },
-    { value: 40, symbol: 'XL' },
-    { value: 10, symbol: 'X' },
-    { value: 9, symbol: 'IX' },
-    { value: 5, symbol: 'V' },
-    { value: 4, symbol: 'IV' },
-    { value: 1, symbol: 'I' }
-  ];
-  
-  let roman = '';
-  
-  for(let i=0; i<romanSymbols.length; i++) {
-    while(num >= romanSymbols[i].value) {
-      roman += romanSymbols[i].symbol;
-      num -= romanSymbols[i].value;
+function intToRoman(num) {
+    const romanNumerals = [
+        { value: 1000, symbol: 'M' },
+        { value: 900, symbol: 'CM' },
+        { value: 500, symbol: 'D' },
+        { value: 400, symbol: 'CD' },
+        { value: 100, symbol: 'C' },
+        { value: 90, symbol: 'XC' },
+        { value: 50, symbol: 'L' },
+        { value: 40, symbol: 'XL' },
+        { value: 10, symbol: 'X' },
+        { value: 9, symbol: 'IX' },
+        { value: 5, symbol: 'V' },
+        { value: 4, symbol: 'IV' },
+        { value: 1, symbol: 'I' }
+    ];
+
+    let result = '';
+
+    for (let i = 0; i < romanNumerals.length; i++) {
+        while (num >= romanNumerals[i].value) {
+            result += romanNumerals[i].symbol;
+            num -= romanNumerals[i].value;
+        }
     }
-  }
-  
-  return roman;
+
+    return result;
 }
+
+// Example usage:
+console.log(intToRoman(14)); // Output: XIV
+console.log(intToRoman(798)); // Output: DCCXCVIII
